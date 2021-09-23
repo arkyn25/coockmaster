097ee0b6daf181071944b283fa1512ab08d85ce2
Arlen Freitas <arlenfreitas1@gmail.com>
2021-09-23 16:37:49 -0300

    quebro tudo

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -28,7 +28,6 @@ const login = async ({ email, password }) => {
   if (!email || !password) {
     throw err('All fields must be filled');
   }
-
   const checkUser = await userModel.getUserByEmail(email);
   if (!checkUser || checkUser.password !== password) {
     throw err('Incorrect username or password');
